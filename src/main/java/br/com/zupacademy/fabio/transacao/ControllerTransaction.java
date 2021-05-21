f16a3c4793b779ff99b4879171c9c5cd3f64b336
Fábio L <fflucas@hotmail.com>
2021-05-21 18:20:49 -0300

    Lista as últimas transações

--- src/main/java/br/com/zupacademy/fabio/transacao/ControllerTransaction.java
+++ src/main/java/br/com/zupacademy/fabio/transacao/ControllerTransaction.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.NotBlank;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/v1/cards/{id_card}/transactions")
@@ -18,11 +19,13 @@ public class ControllerTransaction {
 
     private final LegacyApiTransaction transactionApi;
     private final PropostaApi propostaApi;
-
+    private final RepositoryTransaction repositoryTransaction;
+    private ResponseToRequestApi card;
     @Autowired
-    public ControllerTransaction(LegacyApiTransaction transactionApi, PropostaApi propostaApi) {
+    public ControllerTransaction(LegacyApiTransaction transactionApi, PropostaApi propostaApi, RepositoryTransaction repositoryTransaction) {
         this.transactionApi = transactionApi;
         this.propostaApi = propostaApi;
+        this.repositoryTransaction = repositoryTransaction;
     }
 
     @PostMapping
@@ -31,7 +34,6 @@ public class ControllerTransaction {
             @RequestBody @NotBlank String email,
             @RequestHeader(HttpHeaders.AUTHORIZATION) String token
     ){
-        ResponseToRequestApi card;
         try {
             card = propostaApi.getCard(id_card, token);
         }catch (FeignException.Unauthorized feu){
@@ -60,9 +62,10 @@ public class ControllerTransaction {
             @PathVariable Long id_card,
             @RequestHeader(HttpHeaders.AUTHORIZATION) String token
     ){
-        ResponseToRequestApi card;
         try {
             card = propostaApi.getCard(id_card, token);
+        }catch (FeignException.Unauthorized feu){
+            throw new ApiErrorException(HttpStatus.UNAUTHORIZED, "O servidor de autorização não aceitou o token");
         }catch (FeignException fe){
             fe.printStackTrace();
             throw new ApiErrorException(HttpStatus.BAD_REQUEST, "Algo saiu errado com o servidor de propostas");
@@ -76,5 +79,23 @@ public class ControllerTransaction {
         }
 
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Object> listTen(
+            @PathVariable Long id_card,
+            @RequestHeader(HttpHeaders.AUTHORIZATION) String token
+    ){
+        try {
+            card = propostaApi.getCard(id_card, token);
+        }catch (FeignException.Unauthorized feu){
+            throw new ApiErrorException(HttpStatus.UNAUTHORIZED, "O servidor de autorização não aceitou o token");
+        }catch (FeignException fe){
+            fe.printStackTrace();
+            throw new ApiErrorException(HttpStatus.BAD_REQUEST, "Algo saiu errado com o servidor de propostas");
+        }
+
+        List<Transaction> lastTransactions = repositoryTransaction.findTenByCardIdOrderByMadeInDesc(card.getNumber());
+        return ResponseEntity.ok(lastTransactions);
     }
 }
--- src/main/java/br/com/zupacademy/fabio/transacao/RepositoryTransaction.java
+++ src/main/java/br/com/zupacademy/fabio/transacao/RepositoryTransaction.java
@@ -8,5 +8,5 @@ import java.util.List;
 @Repository
 public interface RepositoryTransaction extends JpaRepository<Transaction, String> {
     // SELECT * FROM transaction t LEFT JOIN card c ON t.card_id = c.id WHERE c.number = "6224-8712-7454-8217" ORDER BY t.made_in DESC LIMIT 10;
-//    List<Transaction> findTenByCardNumberOrderByMadeInDesc(String number);
+    List<Transaction> findTenByCardIdOrderByMadeInDesc(String number);
 }
